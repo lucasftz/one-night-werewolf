4c9a05f565bd87c7e48a6b81bf6629b031818790
Lucas Fietkiewicz <lucas.fietkiewicz@gmail.com>
2022-05-11 18:42:24 -0400

    Make a hello world bot

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("hello world!");
+import "dotenv/config";
+import Discord from "discord.js";
+
+const client = new Discord.Client({
+  intents: ["GUILDS", "GUILD_MESSAGES"],
+});
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user?.tag}`);
+});
+
+client.login(process.env.TOKEN);
