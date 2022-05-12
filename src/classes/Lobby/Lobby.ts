141af50e7a00b20c3186b1ae6f973194dba326f8
Lucas Fietkiewicz <lucas.fietkiewicz@gmail.com>
2022-05-12 12:51:04 -0400

    Restyle the lobby embed

--- src/classes/Lobby/Lobby.ts
+++ src/classes/Lobby/Lobby.ts
@@ -12,13 +12,14 @@ class Lobby {
   addRole: (role: string) => MessageEmbed;
   private _players: string[];
   private _id?: string;
+  private _updateEmbed;
 
   constructor(creator: User) {
     this._id;
     this._players = [];
     this.roles = [];
     this.embed = new MessageEmbed()
-      .setDescription("")
+      .setDescription("One Night Werewolf")
       .setColor([255, 255, 255]);
 
     this.setID = (id: string) => {
@@ -28,57 +29,39 @@ class Lobby {
 
     this.addRole = (role: string) => {
       if (this.roles.length !== this._players.length + 3) this.roles.push(role);
-      const newEmbed = new MessageEmbed()
-        .setTitle(this.embed.title!)
-        .setDescription(this.embed.description!)
-        .setFields([
-          {
-            name: `${this.roles.length} out of ${
-              this._players.length + 3
-            } roles selected`,
-            value: this.roles.join("\n" + "\n") || "No roles selected",
-          },
-        ])
-        .setColor([255, 255, 255]);
-      return newEmbed;
+      return this._updateEmbed();
     };
 
     this.addPlayer = (newPlayer: string) => {
       if (!this._players.includes(newPlayer)) {
         this._players.push(newPlayer);
-        const newEmbed = new MessageEmbed()
-          .setTitle(`Current Players: ${this._players.length}`)
-          .setDescription(this.embed.description + `${newPlayer}\n`)
-          .setFields([
-            {
-              name: `${this.roles.length} out of ${
-                this._players.length + 3
-              } roles selected`,
-              value: this.roles.join("\n" + "\n") || "No roles selected",
-            },
-          ])
-          .setColor([255, 255, 255]);
-        this.embed = newEmbed;
+        this.embed = this._updateEmbed();
       }
       return this.embed;
     };
 
     this.removePlayer = (player: string) => {
       this._players = this._players.filter((oldPlayer) => oldPlayer !== player);
-      const newEmbed = new MessageEmbed()
-        .setTitle(`Current Players: ${this._players.length}`)
-        .setDescription(this._players.join("\n") + "\n")
+      this.embed = this._updateEmbed();
+      return this.embed;
+    };
+
+    this._updateEmbed = () => {
+      return new MessageEmbed()
+        .setDescription("One Night Werewolf")
         .setFields([
+          {
+            name: `Current Players: ${this._players.length}`,
+            value: this._players.join("\n") || "No players in the lobby",
+          },
           {
             name: `${this.roles.length} out of ${
               this._players.length + 3
             } roles selected`,
-            value: this.roles.join("\n" + "\n") || "No roles selected",
+            value: this.roles.join("\n") || "No roles selected",
           },
         ])
         .setColor([255, 255, 255]);
-      this.embed = newEmbed;
-      return this.embed;
     };
 
     this.getPlayers = () => this._players;
